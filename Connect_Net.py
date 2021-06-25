00a9df0ce5bb6fcc46a24782fca4817557383774
caravaned <47841017+caravaned@users.noreply.github.com>
2021-06-24 17:53:24 -0700

    Add files via upload
    校园网使用.4 宿舍网使用.6
    验证网络状态设置为IP地址

--- Connect_Net.py
+++ Connect_Net.py
@@ -1,4 +1,4 @@
-import requests,socket
+import requests, socket
 
 # 在下方输入你的账号
 UserName = ""
@@ -15,30 +15,31 @@ Pwd = ""
 def Check_connected():
     s = socket.socket()
     s.settimeout(2)
-    status = s.connect_ex(('www.baidu.com', 443))
+    status = s.connect_ex(('103.235.46.39', 443))
     return status
 
-def connect_net(UserName,Pwd,ISP):
+def connect_net(UserName,Pwd,ISP,Post_addr):
+
     hostname = socket.gethostname()
     ip = socket.gethostbyname(hostname)
     post_str = "DDDDD=%2C0%2C"+UserName+"%40"+ISP+"&upass="+Pwd+"&R1=0&R2=0" \
               "&R6=0&para=00&0MKKey=123456&buttonClicked=&redirect_url=&err_fl" \
               "ag=&username=&password=&user=&cmd=&Login="
-    post_login = "http://172.168.254.6:801/eportal/?c=ACSetting&a=Login&protoco" \
-                 "l=http:&hostname=172.168.254.6&iTermType=1&mac=000000000000&" \
+    post_login = "http://"+Post_addr+":801/eportal/?c=ACSetting&a=Login&protoco" \
+                 "l=http:&hostname="+Post_addr+"&iTermType=1&mac=000000000000&" \
                  "ip="+ip+"&enAdvert=0&loginMethod=1"
     post_lenth = len(post_str)
     post_header ={
-        'Host': '172.168.254.6:801',
+        'Host': ''+Post_addr+':801',
         'Connection': 'keep-alive',
         'Content-Length': str(post_lenth),
         'Cache-Control': 'max-age=0',
         'Upgrade-Insecure-Requests': '1',
-        'Origin': 'http://172.168.254.6',
+        'Origin': 'http://'+Post_addr+'',
         'Content-Type': 'application/x-www-form-urlencoded',
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'Referer': 'http://172.168.254.6/',
+        'Referer': 'http://'+Post_addr+'/',
         'Accept-Encoding': 'gzip, deflate',
         'Accept-Language': 'zh-CN,zh;q=0.9',
         'Cookie': 'program=aygxy; vlan=0; ip='+ip+'; areaID=mac%3D000000000000; is_login=1'
@@ -69,7 +70,11 @@ def main():
         print("network is already connected")
         pass
     else:
-        connect_net(UserName, Pwd, ISP)
+        if ISP == "":
+            Post_addr = "172.168.254.4"
+        else:
+            Post_addr = "172.168.254.6"
+        connect_net(UserName, Pwd, ISP, Post_addr)
 
 if __name__ == '__main__':
     main()
